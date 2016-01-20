31c0344c6086671b87d594791505b1c9c603be45
Michael Prinz <mnprinz@gmail.com>
2016-01-19 17:54:06 -0700

    made changes to milestone

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<title>Michael Prinz - Web Developer</title>
+	</head>
+	<body>
+		<h2>Purpose</h2>
+			<p>Web developer specializing in HTML, CSS, JavaScript, Angular, MySQL and PHP.</p>
+		<h2>Audience</h2>
+		<h2>Goal</h2>
+	</body>
+</html>
