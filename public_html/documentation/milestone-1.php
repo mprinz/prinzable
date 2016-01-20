b148d0842dabc6918b2f72648c6749096a978c24
Michael Prinz <mnprinz@gmail.com>
2016-01-19 20:34:56 -0700

    made changes to milestone

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -5,9 +5,20 @@
 		<title>Michael Prinz - Web Developer</title>
 	</head>
 	<body>
+
 		<h2>Purpose</h2>
-			<p></p>
+
+			<p>This website is intended be the personal web page of Michael Prinz and a demonstration of his web development skills for potential employers.>
+
 		<h2>Audience</h2>
+
+			<p>The audience for this this website will primarily be experienced web and software developers interested in hiring Michael Prinz.
+		<h3> Persona</h3>
+				Alison Mead is 43 years old. She lives in Denver, Colorado and runs a web developement company.  Her company employs 42 people and is business is quite good
+				Alison is highly experienced in web development and perosnally oversees the hiriing new developers.</p>
+
 		<h2>Goal</h2>
+
+			<p>The goal iof the website is twofold.  First, it will serve as an ongoing showcase of Michael Prinz' capabilities in the primary languages used in full-stack web development: HTML5, CSS JavaScript, MySql and PHP.</p>
 	</body>
 </html>
