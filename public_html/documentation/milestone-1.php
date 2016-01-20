23967e46517739954e6688b1998fa5fbb0a90150
Michael Prinz <mnprinz@gmail.com>
2016-01-20 09:12:16 -0700

    made changes to milestone

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -9,18 +9,18 @@
 
 		<h2>Purpose</h2>
 
-			<p>This website is intended be the personal web page of Michael Prinz and a demonstration of his web development skills for potential employers.>
+			<p>This website is intended be the personal web page of Michael Prinz and a demonstration of his web development skills for potential employers.</p>
 
 		<h2>Audience</h2>
 
-			<p>The audience for this this website will primarily be experienced web and software developers interested in hiring Michael Prinz./p>
+			<p>The audience for this this website will primarily be experienced web and software developers interested in hiring Michael Prinz.</p>
 
 			<h3> Persona</h3>
 					<p>Alison Mead is 43 years old. She lives in Denver, Colorado and runs a web development company.  Her company employs 42 people and business is currently quite good
 					Alison is highly experienced in web development and personally oversees the hiring new developers.</p>
 			<h3>Use Case</h3>
-					<p>Alison's buinsess is doing well and she believes it could expand and become more profitable but it will be necessary to hire new qualified staff.
-					The new developer should have a solid command of HTLM5, CSS, Angular, MySQL and PHP. The job openings have been posted and Michael Prinz responded by emailig his resume and a link to his website.</p>
+					<p>Alison's business is doing well and she believes it could expand and become more profitable but it will be necessary to hire new qualified staff.
+					The new developer should have a solid command of HTLM5, CSS, Angular, MySQL and PHP. The job openings have been posted and Michael Prinz responded by emailing his resume and a link to his website.</p>
 
 		<h2>Goal</h2>
 
