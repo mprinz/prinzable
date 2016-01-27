0dc9f46add687f64a8673953c7f6f3b77ca88a2d
Michael Prinz <mnprinz@gmail.com>
2016-01-27 12:48:07 -0700

    Created Milestone 2 file

---
+++ public_html/documentation/milestone -2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+	<html>
+	<head>
+		<title>Milestone-2</title>
+	</head>
+	<body>
+		<h2>Sitemap</h2>
+		<p>Mike's personal web site will contain 3 pages. They will be: </p>
+		<ul>
+			<li>Welcome page</li>
+			<li>Professional information page</li>
+			<li>Personal information page</li>
+		</ul>
+			<h3>Welcome page</h3>
+				<p>The Welcome page will be the home page and the first page a visitor will see.
+				It will not contain a lot of content and be somewhat minimalist in its appearance.
+				The Welcome page will feature Michael's name in a prominent position and have navigation bar buttons to access the two other pages on the site.
+				There will also be a "contact" section where visitors can leave their name, email and a comment.</p>
+	</body>
+</html>
