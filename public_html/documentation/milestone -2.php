21932671f3826edf0fae8f2da00bae2a9ede6634
Michael Prinz <mnprinz@gmail.com>
2016-01-28 08:18:54 -0700

    created milestone 2

--- public_html/documentation/milestone -2.php
+++ public_html/documentation/milestone -2.php
@@ -1,25 +1,40 @@
 <!DOCTYPE html>
-	<html>
+<html>
 	<head>
 		<title>Milestone-2</title>
 	</head>
 	<body>
-		<h2>Sitemap</h2>
+		<h1>Sitemap</h1>
 		<p>Mike's personal web site will contain 3 pages. They will be: </p>
 		<ul>
 			<li>Welcome page</li>
 			<li>Professional information page</li>
 			<li>Personal information page</li>
 		</ul>
-			<h3>Welcome page</h3>
-				<p>The Welcome page will be the home page and the first page a visitor will see.
-				It will not contain a lot of content and be somewhat minimalist in its appearance.
-				The Welcome page will feature Michael's name in a prominent position and have navigation bar buttons to access the two other pages on the site.
-				There will also be a "contact" section where visitors can leave their name, email and a comment.</p>
-			<h3>Professional information page</h3>
-				<p>The Professional page will essentially contain Mike's resume</p>
-			<h3>Personal information page</h3>
-				<p>The Personal page will contain some personal information about Mike, including a brief biography and his interests/hobbies.
-				There will be some images of Mike's photos and perhaps a drawing he has done.</p>
+		<h2>Welcome page</h2>
+		<p>The Welcome page will be the home page and the first page a visitor will see.
+			It will not contain a lot of content and be somewhat minimalist in its appearance.
+			The Welcome page will feature Michael's name in a prominent position and have navigation bar buttons to access
+			the three other pages on the site.</p>
+		<h3>Professional information page</h3>
+		<p>The Professional page will contain Mike's resume.</p>
+		<h3>Personal information page</h3>
+		<p>The Personal page will contain some personal information about Mike, including a brief biography and his
+			interests/hobbies.
+			There will be some images of Mike's photos and perhaps a drawing he has done.</p>
+		<h2>Content Strategy</h2>
+		<p>The welcome page will have a pleasing background image but this image will be slightly faded so it does not
+			stand out too much.
+			Then will be the name of the designer, me! This will be in the top center of the page. This is intended to draw
+			attention to the name and to convey confidence about it. It will then be followed by a brief biographic
+			statement of rougly 100 characters.
+			Under the bio will be two buttons labeled "professional" and "personal". These buttons will take you to the two
+			other pages of the website.
+		<h3>Professional page</h3>
+		<p>This page will contain Mike's resume but in a modern style.</p>
+		<<h3>Personal page</h3>
+		<p>This page will feature a photo of Mike, probably a headshot. There will be a brief statment about Mike's
+			personal interests and perhaps an interesting experience or story he can share. This is intented to portray him
+			as an interesting and intelligent person who you would want to hire.</p>
 	</body>
 </html>
