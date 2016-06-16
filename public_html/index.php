737566a256b609d569c8f56eb26e5aafcac4e776
Michael Prinz <mnprinz@gmail.com>
2016-06-16 15:41:15 -0600

    created head.html

--- public_html/index.php
+++ public_html/index.php
@@ -1,37 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<meta http-equiv= "Content-Type" content="text/html; charset=UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<meta description=" "
-
-		<!-- Latest compiled and minified CSS -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-
-		<!-- Optional theme -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
-
-		<!--Font awesome-->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-
-		<!--CSS style sheet-->
-		<link type="text/css" rel="stylesheet" href="css/style.css">
-		<link rel="stylesheet" href="css/animate.min.css">
-
-		<!-- jQuery -->
-		<script src="https://code.jquery.com/jquery-2.2.3.min.js"   integrity="sha256-a23g1Nt4dtEYOj7bR+vTu7+T8VP13humZFBJNIYoEJo="   crossorigin="anonymous"></script>
-
-		<!-- Latest compiled and minified JavaScript -->
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-
-		<!-------Favicon------>
-		<link rel="shortcut icon" href="/images/favicon.ico" />
-
-		<script src="js/custom.js" rel="text/javascript"></script>
-		<script src="js/script.js" rel="text/javascript"></script>
-		<title>Michael Prinz</title>
-
-	</head>
+	<link type="text/html" rel="head" href="php/head.html">
 	<body class="sfooter">
 		<div class="sfooter-content">
 			<!--nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
@@ -76,7 +45,6 @@
 						<h2 class="text-center">Web Developer | Santa Fe</h2>
 					</div>
 				</div>
-
 			<nav class="navbar nav-center">
 				<ul class="nav nav-pills">
 					<li role="presentation" class="inactive"><a href="#">Home</a></li>
