b805771b59c12ca95cea83e829ba713d362f3cbd
Michael Prinz <mnprinz@gmail.com>
2016-06-28 15:09:20 -0600

    occidental image

--- public_html/index.php
+++ public_html/index.php
@@ -47,7 +47,7 @@
 					</div>
 				</div>
 			</nav-->
-			<div class="parallax-window" data-parallax="scroll" data-image-src="/images/hubble_friday.jpg">
+			<div class="parallax-window" data-parallax="scroll" data-image-src="/images/.jpg">
 				<div class = "jumbotron">
 					<div class = "bg"></div>
 					<div class = "container">
